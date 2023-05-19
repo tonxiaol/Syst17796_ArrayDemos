0b4e053c93343ad94e822a9b1b5562705356df03
Xiaoling Tong <tonxiaol@sheridancollege.ca>
2023-05-19 13:33:29 -0400

    Revert "added 3rd commit"
    This reverts commit ed17ea0209af12c79b01eb568cd192dfdf3afc45.

--- src/arraysdemo/StudentList.java
+++ src/arraysdemo/StudentList.java
@@ -5,11 +5,10 @@ public class StudentList {
     public static void main(String[] args){
         Student s1 = new Student("s1", "ronak");
         
-        Student[] studentList = new Student[4];
+        Student[] studentList = new Student[3];
         studentList[0] = s1;
         studentList[1] = new Student("s2", "abc");
         studentList[2] = new Student("s3", "def");
-        studentList[3] = new Student("s4", "ghi");
         
         
         for(int i = 0; i < studentList.length; i++){
